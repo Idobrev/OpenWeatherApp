b90510e966f11b1380c29dfe05459f12c1787ad0
Idobrev <i.nikolaev.d@gmail.com>
2015-11-30 23:33:04 +0200

    Fixing select shit

--- JS/select.js
+++ JS/select.js
@@ -6,7 +6,7 @@ $('#select').each(function(){
     $this.after('<div class="select-styled"></div>');
 
     var $styledSelect = $this.next('div.select-styled');
-    $styledSelect.html($this.children('option').eq(0).text() + '<div id="arrow"></div>');
+    $styledSelect.html('<div id="selected">' + $this.children('option').eq(0).text() + '</div><div id="arrow"></div>');
   
     var $list = $('<ul />', {
         'class': 'select-options'
@@ -34,7 +34,7 @@ $('#select').each(function(){
   
     $listItems.click(function(e) {
         e.stopPropagation();
-        $styledSelect.text($(this).text()).removeClass('active');
+        $("#selected").text($(this).text()).removeClass('active');
         $this.val($(this).attr('rel'));
         $list.hide();
         //console.log($this.val());
